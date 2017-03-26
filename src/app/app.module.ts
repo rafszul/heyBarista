68779d5d1d5ad3e1b0686b167a36a7c2086f0e79
rafszul <rafszul@gmail.com>
2017-03-26 03:24:18 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import 'hammerjs';
+import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MdlModule } from '@angular-mdl/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +19,12 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //Material
+  MaterialModule.forRoot(),
+  //Flex
+  FlexLayoutModule,
+  MdlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
