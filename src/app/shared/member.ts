be7119a78debafdd78c894ba14b8af1fed0ecdf4
rafszul <rafszul@gmail.com>
2017-03-29 09:15:49 +0100

    member class

---
+++ src/app/shared/member.ts
@@ -0,0 +1,18 @@
+export class Member {
+    id: number;
+    img: string;
+    imgsmall: string;
+    name: number;
+    firstname: string;
+    lastname: string;
+    email: number;
+    phone: string;
+    website: string;
+    twitter: number;
+    instagram: string;
+    facebook: string;
+    profession: string;
+    aboutshort: number;
+    about: string;
+}
+
